63d1d99103d0c00d6f03982c8c6dc79cfcf03706
Ljiljana Trajkovska <ljiljanatrajkovska@Ljiljanas-MacBook-Pro.local>
2020-12-17 18:50:34 +0100

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const app = () => (
   <>
-    <h1>Welcome to React Parcel Micro App!</h1>
+    <h1>Welcome to React Parcel Micro App!!!</h1>
     <p>Hard to get more minimal than this React app.</p>
   </>
 );
